2653722d2678b74ed908444dda3e21d18350e711
Steven Dunn <Dunn.Steven1@gmail.com>
2018-08-27 11:35:21 -0700

    Added base scss file import.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'home',
+  selector: 'loj-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
+  upsConnectionStatus: string;
+  fedexConnectionStatus: string;
+  dhlConnectionStatus: string;
+  uspsConnectionStatus: string;
+  otherConnectionStatus: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.upsConnectionStatus = 'Connected';
+    this.fedexConnectionStatus = 'Connected';
+    this.dhlConnectionStatus = 'Connection Failure';
+    this.uspsConnectionStatus = 'Not Connected';
+    this.otherConnectionStatus = 'Connection Coming Soon';
   }
 
+  getStatusClass = (connectionStatus: string) => {
+    if (connectionStatus === 'Connected') {
+      return 'connected';
+    }
+    if (connectionStatus === 'Connection Failure') {
+      return 'failure';
+    }
+    if (connectionStatus === 'Connection Coming Soon') {
+      return 'disabled';
+    }
+    else {
+      return 'disconnected';
+    }
+  };
+
+  setStatusClass = (connectionType: string) => {
+    switch (connectionType) {
+      case 'ups':
+        return this.getStatusClass(this.upsConnectionStatus);
+      case 'fedex':
+        return this.getStatusClass(this.fedexConnectionStatus);
+      case 'dhl':
+        return this.getStatusClass(this.dhlConnectionStatus);
+      case 'usps':
+        return this.getStatusClass(this.uspsConnectionStatus);
+      default:
+        return this.getStatusClass(this.otherConnectionStatus);
+    }
+  };
 }
