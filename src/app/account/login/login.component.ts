0b7b6da24a7a06f307536684621f31842575d220
Steven Dunn <Dunn.Steven1@gmail.com>
2018-09-14 17:23:02 -0700

    Added a basic ng-bootstrap modal to test imports.

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  addCompanyModal: NgbModalRef;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
+
+  showAddCompanyModel(content: any) {
+    this.addCompanyModal = this.modalService.open(content, {
+      windowClass: 'loj-bootstrap-modal add-company-modal',
+      backdrop: false
+    });
+
+    // Handle closing the modal
+    this.addCompanyModal.result.then(
+      // Model closed with .close() (i.e. a button)
+      () => {
+        // Do Nothing
+      },
+      // Model closed with backdrop click or Escape Key
+      () => {
+        // Do Nothing
+      });
+  }
+
+  closeAddCompanyModal = () => {
+    let someData = 'you closed the Add Company model';
+    // modalRef is an NgbModalRef. Returns some data when the model is closed.
+    if (this.addCompanyModal !== null) {
+      this.addCompanyModal.close(someData);
+    }
+    // If you opened a component directly, then this would give you access to the component.
+    // this.modalRef.componentInstance
+  };
 
 }
