94d4a970879fd5808995408cedfd9a86caf061d4
Steven Dunn <Dunn.Steven1@gmail.com>
2023-08-22 11:35:49 -0700

    Updated to Angular 16.2.1. The CLI auto-fixed the auth garud by removing CanActivate.

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
